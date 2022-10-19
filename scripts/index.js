383bd7eca8dfca2eeb0cfcc307189332c8695cf0
Siim-Morten Ojasalu <siim.ojasalu@gmail.com>
2022-10-20 00:35:04 +0300

    Banner slides

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,31 @@
 $(document).ready(function () {
-    // Just a test func
-    //$("button").click(function () {
-    //    $("p").hide();
-    //});
+    $('.switch-1').on('click', function() {
+        if ($('.banner-1').is(':visible')) {
+            return;
+        }
+        $('.switch').css('background-color', '#bbb');
+        $(this).css('background-color', '#777777');
+        $('.banner').hide();
+        $('.banner-1').fadeIn();
+    });
+
+    $('.switch-2').on('click', function() {
+        if ($('.banner-2').is(':visible')) {
+            return;
+        }
+        $('.switch').css('background-color', '#bbb');
+        $(this).css('background-color', '#777777');
+        $('.banner').hide();
+        $('.banner-2').fadeIn();
+    });
+
+    $('.switch-3').on('click', function() {
+        if ($('.banner-3').is(':visible')) {
+            return;
+        }
+        $('.switch').css('background-color', '#bbb');
+        $(this).css('background-color', '#777777');
+        $('.banner').hide();
+        $('.banner-3').fadeIn();
+    });
 });
